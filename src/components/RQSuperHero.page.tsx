a96cfa39f215574ddd4f9c49a15b0fa543553f1c
Ravikumar Badami <ravibadami191@gmail.com>
2026-06-21 23:30:22 +0530

    React query cache completed

--- src/components/RQSuperHero.page.tsx
+++ src/components/RQSuperHero.page.tsx
@@ -2,13 +2,15 @@ import {useQuery} from '@tanstack/react-query';
 import axios from 'axios'
 
 function RQSuperHeroPage() {
-    const {isLoading, data,isError,error} =useQuery({
-        queryKey:['rq-super-heros'],
-        queryFn:()=>{
-            return axios.get("http://localhost:4000/superheroes")
-            },
-        }
-       )
+  const { isLoading, data, isError, error, isFetching } = useQuery({
+    queryKey: ['rq-super-heros'],
+    queryFn: () => {
+      return axios.get('http://localhost:4000/superheroes')
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes fresh
+    gcTime: 1000 * 60 * 10,   // keep cache for 10 minutes
+  })
+     if(isFetching) return <h2>{error.message}</h2>
    if(isError) return <h2>{error.message}</h2>
    if(isLoading) return <h2>..Loading</h2>
    console.log(data)
