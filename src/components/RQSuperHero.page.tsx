eeacfa8bbfcde97b9f6d8727aa576e02dbc8b0d7
Ravikumar Badami <ravibadami191@gmail.com>
2026-06-23 11:22:13 +0530

    react query fetch on click

--- src/components/RQSuperHero.page.tsx
+++ src/components/RQSuperHero.page.tsx
@@ -2,22 +2,23 @@ import {useQuery} from '@tanstack/react-query';
 import axios from 'axios'
 
 function RQSuperHeroPage() {
-  const { isLoading, data, isError, error, isFetching } = useQuery({
+  const { isLoading, data, isError, error, isFetching,refetch } = useQuery({
     queryKey: ['rq-super-heros'],
     queryFn: () => {
       return axios.get('http://localhost:4000/superheroes')
     },
-    staleTime: 1000 * 60 * 5, // 5 minutes fresh
-    gcTime: 1000 * 60 * 10,   // keep cache for 10 minutes
+enabled:false,
+
   })
-     if(isFetching) return <h2>{error.message}</h2>
+     if(isFetching) return <h2>...loading</h2>
    if(isError) return <h2>{error.message}</h2>
    if(isLoading) return <h2>..Loading</h2>
    console.log(data)
   return(
    <>
    <h1>RQ Super Heros Page</h1>
-   {data.data.map((hero)=>{
+   <button onClick={refetch}>Refetch</button>
+   {data?.data.map((hero)=>{
        return <div key={hero.id} >{hero.name}</div>
        })}
    </>
